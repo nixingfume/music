3f5e29807a76858de012d76a3e0f917ff1ce5816
liqiu <878506361@qq.com>
2017-07-18 10:20:13 +0800

    音乐播放器的推荐页面

---
+++ src/components/singer/singer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>歌手页面</div>
+</template>
+<style lang="stylus" type="text/stylus" rel="stylesheet/stylus">
+</style>
+<script>
+    export default{
+        props: {},
+        data(){
+            return {}
+        },
+        watch: {}
+    }
+</script>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+import fastclick from 'fastclick'
+import router from './router'
+import 'common/stylus/index.styl'
+import VueLazyLoad from 'vue-lazyload'
+
+Vue.use(VueLazyLoad,{
+  loading:require('common/images/default.png')
+})
+fastclick.attach(document.body)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
